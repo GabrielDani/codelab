c75ec8b8e796dcafe5999fbfcf82897f2c59f368
Gabriel Dani <gabriel.dani07@gmail.com>
2025-07-10 18:18:45 -0300

    feat: initial clerk use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from "@clerk/localizations";
 
 import "@/styles/globals.css";
-import { cn } from "@/lib/utils";
 
 const nunito = Nunito({
   variable: "--font-sans",
@@ -19,10 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" suppressHydrationWarning>
-      <body className={cn(nunito.variable, "antialiased font-sans dark")}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      localization={ptBR}
+      appearance={{ variables: { colorPrimary: "hsl(160 100% 37%)" } }}
+    >
+      <html lang="pt-br" suppressHydrationWarning>
+        <body className={cn(nunito.variable, "antialiased font-sans dark")}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
