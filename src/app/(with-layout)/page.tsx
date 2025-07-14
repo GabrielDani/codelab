a62c418f58d68cf895bb76e3e8af1c50a142366f
Gabriel Dani <gabriel.dani07@gmail.com>
2025-07-14 14:48:10 -0300

    feat: initial course page and search prisma courses

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,9 +1,20 @@
+import { CoursesList } from "@/components/pages/courses/courses-list";
 import { CourseTagsList } from "@/components/pages/courses/tags-list";
 
-export default function Home() {
+type CoursesPageProps = {
+  searchParams: Promise<{
+    query: string;
+    tags: string | string[];
+  }>;
+};
+
+export default async function CoursesPage({ searchParams }: CoursesPageProps) {
+  const { query, tags } = await searchParams;
+
   return (
     <>
       <CourseTagsList />
+      <CoursesList query={query} tags={tags} />
     </>
   );
 }
