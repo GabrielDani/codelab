82425d8d1d14c6c6f43fc9d0b5eca7b7fabd6525
Gabriel Dani de Azevedo <gabriel.dani07@gmail.com>
2025-07-15 12:54:35 -0300

    feat: course details page added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,45 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { CourseDifficulty } from "@/generated/prisma";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatDuration = (durationInMs: number, showHours = false) => {
+  const hours = Math.floor(durationInMs / (1000 * 60 * 60));
+  const minutes = Math.floor((durationInMs % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((durationInMs % (1000 * 60)) / 1000);
+
+  const formatNumber = (num: number) => num.toString().padStart(2, "0");
+
+  if (hours > 0 || showHours) {
+    return (
+      formatNumber(hours) +
+      ":" +
+      formatNumber(minutes) +
+      ":" +
+      formatNumber(seconds)
+    );
+  } else {
+    return formatNumber(minutes) + ":" + formatNumber(seconds);
+  }
+};
+
+export const formatDifficulty = (difficulty: CourseDifficulty) => {
+  switch (difficulty) {
+    case CourseDifficulty.EASY:
+      return "Iniciante";
+    case CourseDifficulty.MEDIUM:
+      return "Intermediário";
+    case CourseDifficulty.HARD:
+      return "Avançado";
+  }
+};
+
+export const formatPrice = (price: number) => {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(price);
+};
