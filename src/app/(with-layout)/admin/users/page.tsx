99cae7f190c5b46b52d2ec6b70306a21c3e9caf3
Gabriel Dani de Azevedo <gabriel.dani07@gmail.com>
2025-08-04 19:41:33 -0300

    feat: initial admin user page

---
+++ src/app/(with-layout)/admin/users/page.tsx
@@ -0,0 +1,13 @@
+import { getAdminUsers } from "@/actions/user";
+import { UsersTable } from "@/components/pages/admin/users-table";
+
+export default async function AdminUsersPage() {
+  const users = await getAdminUsers();
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Usuário</h1>
+      <UsersTable users={users} />
+    </>
+  );
+}
---
+++ src/components/pages/admin/users-table.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { Avatar } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Input } from "@/components/ui/input";
+import { formatName } from "@/lib/utils";
+import { ColumnDef } from "@tanstack/react-table";
+import { format } from "date-fns";
+import { useMemo, useState } from "react";
+
+type UsersTableProps = {
+  users: AdminUser[];
+};
+
+export const UsersTable = ({ users }: UsersTableProps) => {
+  const [search, setSearch] = useState<string>("");
+
+  const filteredUsers = useMemo(() => {
+    const lowerSearch = search.toLowerCase();
+    return users.filter((user) => {
+      const nameMatch = formatName(user.firstName, user.lastName)
+        .toLowerCase()
+        .includes(lowerSearch);
+      const emailMatch = user.email.toLowerCase().includes(lowerSearch);
+
+      return nameMatch || emailMatch;
+    });
+  }, [search, users]);
+
+  const columns: ColumnDef<AdminUser>[] = [
+    {
+      header: "Nome",
+      accessorKey: "name",
+      cell: ({ row }) => {
+        const user = row.original;
+        const fullName = formatName(user.firstName, user.lastName);
+
+        return (
+          <div className="flex items-center gap-2 p-2">
+            <Avatar src={user.imageUrl} fallback={fullName} />
+            <p className="font-medium">{fullName}</p>
+          </div>
+        );
+      },
+    },
+    {
+      header: "Email",
+      accessorKey: "email",
+    },
+    {
+      header: "Cursos Comprados",
+      accessorKey: "purchasedCourses",
+    },
+    {
+      header: "Aulas Concluídas",
+      accessorKey: "completedLessons",
+    },
+    {
+      header: "Data de Criação",
+      accessorKey: "createdAt",
+      cell: ({ row }) => {
+        const user = row.original;
+        return format(user.createdAt, "dd/MM/yyyy HH:mm");
+      },
+    },
+  ];
+
+  return (
+    <>
+      <div className="w-full flex items-center justify-between gap-4">
+        <Input
+          placeholder="Buscar usuário"
+          value={search}
+          className="max-w-[400px]"
+          onChange={({ target }) => setSearch(target.value)}
+        />
+        <Button>Enviar Notificação</Button>
+      </div>
+
+      <DataTable columns={columns} data={filteredUsers} />
+    </>
+  );
+};
