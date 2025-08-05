1a836439b7503d3f1fc78211b66ec76f59d7e314
Gabriel Dani de Azevedo <gabriel.dani07@gmail.com>
2025-08-05 17:27:54 -0300

    feat: dynamic pages with force-dynamic and revalidate

--- src/app/(with-layout)/admin/page.tsx
+++ src/app/(with-layout)/admin/page.tsx
@@ -1,6 +1,8 @@
 import { getNewPurchasedCoursesStats, getNewUsersStats } from "@/actions/stats";
 import { StatsCharts } from "@/components/pages/admin/stats-charts";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminPage() {
   const newUsers = await getNewUsersStats();
   const newPurchasedCourses = await getNewPurchasedCoursesStats();
