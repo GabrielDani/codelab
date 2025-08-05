0c470d0c329d581364b950d16a831f2120a910f9
Gabriel Dani de Azevedo <gabriel.dani07@gmail.com>
2025-08-05 15:31:41 -0300

    feat: notification by comment

--- src/actions/course-comments.ts
+++ src/actions/course-comments.ts
@@ -3,6 +3,7 @@
 import { prisma } from "@/lib/prisma";
 import { getUser } from "./user";
 import { checkRole } from "@/lib/clerk";
+import { formatName } from "@/lib/utils";
 
 export const getLessonComments = async (lessonId: string) => {
   await getUser();
@@ -42,7 +43,7 @@ export const createLessonComment = async ({
   lessonId,
   parentId,
 }: CreateLessonCommentPayload) => {
-  const { userId } = await getUser();
+  const { userId, user } = await getUser();
 
   if (content.length > 500)
     throw new Error("Comentário deve ter no máximo 500 caracteres");
@@ -83,6 +84,31 @@ export const createLessonComment = async ({
   });
 
   // TODO: notificar usuários
+  if (parentId) {
+    const parentComment = await prisma.lessonComment.findUnique({
+      where: {
+        id: parentId,
+      },
+    });
+
+    const parentUserId = parentComment?.userId;
+
+    if (parentUserId && parentUserId !== userId) {
+      await prisma.notification.create({
+        data: {
+          userId: parentUserId,
+          title: "Nova resposta ao seu comentário",
+          content: `${formatName(
+            user.firstName,
+            user.lastName
+          )} respondeu ao seu comentário: "${content}" no curso ${
+            course.title
+          }`,
+          link: `/courses/${courseSlug}/${lesson.moduleId}/lesson/${lesson.id}`,
+        },
+      });
+    }
+  }
 
   return comment;
 };
